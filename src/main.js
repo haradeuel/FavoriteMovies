21365c546f338a671576d89d77910a87ea90d656
haradeuel <haradeuel@gmail.com>
2024-06-19 17:23:56 -0300

    add validation

---
+++ src/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $("#add-button").on("click", function (event) {
+    event.preventDefault();
+
+    let isValid = false;
+    const name = $("#name").val().trim();
+    const description = $("#description").val().trim();
+
+    if (name === "") {
+      $("#name").addClass("border-red-500");
+      $("#name_error").removeClass("invisible");
+      isValid = false;
+    } else {
+      $("#name").removeClass("border-red-500");
+      $("#name_error").addClass("invisible");
+      isValid = true;
+    }
+
+    if (description === "") {
+      $("#description").addClass("border-red-500");
+      $("#description_error").removeClass("invisible");
+      isValid = false;
+    } else {
+      $("#description").removeClass("border-red-500");
+      $("#description_error").addClass("invisible");
+      isValid = true;
+    }
+
+    if(isValid){
+        console.log("Válido");
+
+        window.location.href="lista_filmes.html";
+    }
+
+    console.log(name);
+    console.log(description);
+  });
+});
