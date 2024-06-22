2a61d88dbcdd53b658bba1e282dc34e81f4453be
haradeuel <haradeuel@gmail.com>
2024-06-22 15:17:43 -0300

    improve desig, add loading

--- src/list.js
+++ src/list.js
@@ -1,21 +1,29 @@
 $(document).ready(function () {
+  // Quando o documento estiver pronto, executar a função
   $.get("http://localhost:3000/movies", function (movies) {
-    const list = $("#list");
+    // Fazer uma requisição GET para obter os filmes da URL local
+
+    $("#loading").hide();
+
+    const list = $("#list"); // Seleciona o elemento com id "list" onde os filmes serão adicionados
 
     movies.forEach(function (movie) {
+      // Itera sobre cada filme retornado pela requisição
+
       list.append(`
-        <div class="w-full flex flex-col gap-4 pt-2">            
-            <div class="w-full">
-                <h2 class="text-xl">${movie.name}</h2>  
+        <div class="max-w-sm w-full max-w-full mb-5">
+          <div class="border-r border-b border border-gray-400 bg-white rounded p-4 flex flex-col justify-between leading-normal">
+            <div class="mb-8">
+              <div class="text-gray-900 font-bold text-xl mb-2">${movie.name}</div>
+              <p class="text-gray-700 text-base">${movie.description}</p>
             </div>
 
-            <div class="">
-                <h2 class="text-xl">${movie.rating ?? "Sem Nota"}</h2>
-            </div>    
-
-            <div class="500">
-                <p>${movie.description}</p>
+            <div class="flex items-center">
+                <div class="text-sm">
+                <p class="text-gray-900 leading-none">Nota: ${movie.rating}</p>                
+              </div>
             </div>
+          </div>
         </div>
       `);
     });
